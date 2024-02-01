f31645915acffd436ee113a00422fefba13095ce
jayNeoverce <jay.parmar.neoverce@gmail.com>
2024-02-01 17:42:45 +0530

    add users

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+  {
+    name: "jay",
+    email: "jayparmar@gmail.com",
+    id: 1,
+  },
+  {
+    name: "pratik",
+    email: "pratik@gmail.com",
+    id: 2,
+  },
+  {
+    name: "Abhay",
+    email: "abhay@gmail.com",
+    id: 3,
+  },
+];
